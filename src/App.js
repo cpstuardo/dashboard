57ce2915f10dbc5f9e3a71e220d234f880b54560
cpstuardo <cpstuardo@uc.cl>
2021-12-31 09:51:47 -0300

    dash

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, Fragment } from "react";
+import "./App.css";
+import { Container, Row, Col } from "reactstrap";
+import BarPlot from "./components/barPlot";
+import PiePlot from "./components/piePlot";
+import dataPie from "./data/dataPie";
+import dataBar from "./data/dataBar";
 
 function App() {
+  const [width, setWindowWidth] = useState(0);
+
+  useEffect(() => {
+    updateDimensions();
+    window.addEventListener("resize", updateDimensions);
+    return () => window.removeEventListener("resize", updateDimensions);
+  }, []);
+
+  const updateDimensions = () => {
+    const width = window.innerWidth;
+    setWindowWidth(width);
+  };
+
+  const barKeys = ["hot dog", "burger", "sandwich", "kebab", "fries", "donut"];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Dashboard</h1>
+      <Fragment>
+        <Container className="app-container">
+          <Row
+            style={{
+              flex: 1,
+              display: "flex",
+              flexDirection: width > 900 ? "row" : "column",
+            }}
+          >
+            <Col style={{ flex: 1 }}>
+              <PiePlot data={dataPie} screenWidth={width} />
+            </Col>
+            <Col style={{ flex: 1 }}>
+              <BarPlot
+                data={dataBar}
+                keys={barKeys}
+                indexBy={"country"}
+                axisXLegend={""}
+                axisYLegend={""}
+                screenWidth={width}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </Fragment>
     </div>
   );
 }
 
 export default App;
+
+//<PiePlot data={dataPie} screenWidth={width} />
